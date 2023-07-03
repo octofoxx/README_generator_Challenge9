947dc2e9f4651a007228ac0cf9f2c407dd6b3b3e
octofoxx <devilmaycry87x@gmail.com>
2023-07-02 19:57:00 -0700

    added code for license badge, section, ToC and link to

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !=='none'){
+    return `
+    ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+      return '';
+    }
+  }
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !=='none'){
+    return `
+    [${license}](https://choosealicense.com/licenses/${license})
+    `;
+  } else {
+      return '';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'none') {
+    return `
+    ## [License](#table-of-contents)
+  
+    The application is covered under the following license:
+  
+    ${renderLicenseLink(license)}
+      `;
+    } else {
+      return ' ';
+    }
+}
+
+ // Function that returns license in table of contents
+ // If there is no license, return an empty string
+ function renderLicenseTOC(license) {
+  if (license !== 'none') {
+  return `
+  * [License](#license)
+    `;
+  } else {
+    return ' ';
+  }
+ }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
 # ${data.title}
+
+${renderLicenseBadge(data.license)}
 
 ## Description
 
@@ -23,6 +66,7 @@ function generateMarkdown(data) {
   
 * [Installation](#installation)
 * [Usage](#usage)
+${renderLicenseTOC(data.license)}
 * [Contributing](#contributing)
 * [Tests](#tests)
 * [Questions](#questions)
@@ -34,6 +78,8 @@ function generateMarkdown(data) {
 ## [Usage](#table-of-contents)
   
   ${data.usage}
+
+${renderLicenseSection(data.license)}  
 
 ## [Contributing](#table-of-contents)
   
